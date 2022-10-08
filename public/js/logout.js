5fa2992d49303d35888b8aaae14bd8477d832b6d
unknown <nvmaxx@gmail.com>
2022-10-08 18:00:31 -0500

    created css and logout js, working on main handlebars need maindashboard created

---
+++ public/js/logout.js
@@ -0,0 +1,17 @@
+// logout function for the logout link
+
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert(response.statusText);
+    }
+}
+
+// event listener on logout link to call logout function
+document.querySelector('#logout').addEventListener('click', logout);
